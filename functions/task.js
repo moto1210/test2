2058348daed930c4f455da2ea6055621061e88cb
apple <c1200922@st.kanazawa-it.ac.jp>
2023-12-02 20:41:19 +0900

    task

--- functions/task.js
+++ functions/task.js
@@ -1,32 +1,33 @@
-function handle_Task() {
-    console.log('こんにちわわ');
+function handle_Task(event, client, userStates) {
+  const userId = event.source.userId;
+  if (!userStates[userId]) {
+      userStates[userId] = { stage: 'idle' };
   }
-  //     const userId = event.source.userId;
-  //     if (!userStates[userId]) {
-  //         userStates[userId] = { stage: 'idle' };
-  //     }
-  
-  //     switch (userStates[userId].stage) {
-  //         case 'idle':
-  //             userStates[userId] = { stage: 'waiting_for_subject' };
-  //             return client.replyMessage(event.replyToken, { type: 'text', text: '科目名を入力してください。' });
 
-  //         case 'waiting_for_subject':
-  //             userStates[userId] = { stage: 'waiting_for_assignment', subject: event.message.text };
-  //             return client.replyMessage(event.replyToken, { type: 'text', text: '課題の内容を入力してください。' });
-  
-  //         case 'waiting_for_assignment':
-  //             userStates[userId] = { ...userStates[userId], stage: 'waiting_for_due_date', assignment: event.message.text };
-  //             return client.replyMessage(event.replyToken, { type: 'text', text: '提出日時を入力してください。' });
-  
-  //         case 'waiting_for_due_date':
-  //             // ここで課題データを保存するロジックを追加
-  //             userStates[userId] = { stage: 'idle' };
-  //             return client.replyMessage(event.replyToken, { type: 'text', text: '課題が追加されました。' });
-  
-  //         default:
-  //             return client.replyMessage(event.replyToken, { type: 'text', text: 'エラーが発生しました。' });
-  //     }
-  // }
-  
-  module.exports = { handle_Task };
+  switch (userStates[userId].stage) {
+      case 'idle':
+          userStates[userId] = { stage: 'waiting_for_subject' };
+          return client.replyMessage(event.replyToken, { type: 'text', text: '科目名を入力してください。' });
+
+      case 'waiting_for_subject':
+          // 科目名を保存
+          userStates[userId] = { ...userStates[userId], stage: 'waiting_for_assignment', subject: event.message.text };
+          return client.replyMessage(event.replyToken, { type: 'text', text: '課題の内容を入力してください。' });
+
+      case 'waiting_for_assignment':
+          // 課題の内容を保存
+          userStates[userId] = { ...userStates[userId], stage: 'waiting_for_due_date', assignment: event.message.text };
+          return client.replyMessage(event.replyToken, { type: 'text', text: '提出日時を入力してください。' });
+
+      case 'waiting_for_due_date':
+          // 提出日時を保存し、課題データを保存するロジックを追加
+          userStates[userId] = { ...userStates[userId], due_date: event.message.text, stage: 'idle' };
+          // ここで課題データを保存するロジックを追加
+          return client.replyMessage(event.replyToken, { type: 'text', text: '課題が追加されました。' });
+
+      default:
+          return client.replyMessage(event.replyToken, { type: 'text', text: 'エラーが発生しました。' });
+  }
+}
+
+module.exports = { handle_Task };
