108b6a3927a9ac89cdd4b90b8afcbfcfd02788ba
HasegawaYuya <c1219854@st.kanazawa-it.ac.jp>
2023-12-02 21:29:42 +0900

    task.js修正

--- functions/task.js
+++ functions/task.js
@@ -10,19 +10,15 @@ function handle_Task(event, client, userStates) {
           return client.replyMessage(event.replyToken, { type: 'text', text: '科目名を入力してください。' });
 
       case 'waiting_for_subject':
-          // 科目名を保存
           userStates[userId] = { ...userStates[userId], stage: 'waiting_for_assignment', subject: event.message.text };
           return client.replyMessage(event.replyToken, { type: 'text', text: '課題の内容を入力してください。' });
 
       case 'waiting_for_assignment':
-          // 課題の内容を保存
           userStates[userId] = { ...userStates[userId], stage: 'waiting_for_due_date', assignment: event.message.text };
           return client.replyMessage(event.replyToken, { type: 'text', text: '提出日時を入力してください。' });
 
       case 'waiting_for_due_date':
-          // 提出日時を保存し、課題データを保存するロジックを追加
           userStates[userId] = { ...userStates[userId], due_date: event.message.text, stage: 'idle' };
-          // ここで課題データを保存するロジックを追加
           return client.replyMessage(event.replyToken, { type: 'text', text: '課題が追加されました。' });
 
       default:
